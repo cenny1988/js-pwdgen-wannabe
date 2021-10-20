3f0ec6cec82fa87fc1c08339fff81a98ba5c1140
cenny1988 <59100992+cenny1988@users.noreply.github.com>
2021-10-20 14:58:07 +0200

    es. funzionante

---
+++ js/script.js
@@ -0,0 +1,26 @@
+// sercizio di oggi: (insicurissimo) password generator
+// nome repo: js-pwdgen-wannabe
+
+
+// Chiedi all’utente il suo nome
+let yourName = prompt("Please enter your name")
+
+// poi chiedi il suo cognome
+let yourSurname = prompt("Please enter your surname")
+
+// poi chiedi il suo colore preferito
+let yourFavoriteColor = prompt("Please enter your favorite color now")
+
+// Debug in console per output
+console.log(yourName)
+console.log(yourSurname)
+console.log(yourFavoriteColor)
+
+// Infine scrivi sulla pagina 
+//“La password assegnata è: 
+// nomecognomecolorepreferito21"
+document.getElementById("my_box").innerHTML = 
+`La password assegnata &egrave;:
+ ${yourName}${yourSurname}${yourFavoriteColor}21
+ `
+ 
